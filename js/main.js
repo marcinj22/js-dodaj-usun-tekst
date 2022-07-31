2241117e210a71e7dcf6e1d99c02d5a5d7e730c0
marcinj <marcin.jurewicz@gmail.com>
2022-07-31 08:01:00 +0200

    wykonanie zadania #39418

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let button1 = document.getElementById('dodaj');
+let button2 = document.getElementById('usun');
+let text = document.getElementById('tekst');
+
+button1.onclick = function () {
+    text.innerHTML = "Pewnego dnia młody programista obudził się wcześnie rano i wyjrzał przez okno...";
+} 
+
+button2.onclick = function () {
+    text.remove();
+}
